7b1f93cc7cfb968b2d0604ae10b2eecacca32f40
Haonan Sun <saospark@br018.pvt.bridges.psc.edu>
2019-02-10 15:26:36 -0500

    update modules

--- scripts/trinity.sh
+++ scripts/trinity.sh
@@ -5,9 +5,17 @@ set -e
 read1=$1
 read2=$2
 outputdir=$3
-module load trinity
+module load gcc/5.3.0
+module load perl/5.18.4-threads
+module load java/jdk8u73
+module load bowtie2/2.2.7
+module load samtools/1.3
+module load jellyfish2/2.2.6
+module load salmon/0.9.1
+module load blat/v35
+module load trinity/2.8.4
 
-trinity --no_version_check --seqType fq --max_memory 16G --CPU 4 \
+Trinity --no_version_check --seqType fq --max_memory 16G --CPU 4 \
   --workdir $LOCAL/trinity_local \
   --output ${outputdir} \
   --left ${read1} \
