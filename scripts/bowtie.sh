fb6de09fea3a82149c0403df308f984941e39474
Kircheis17 <yuhengz@andrew.cmu.edu>
2019-02-13 17:11:01 -0500

    output unmapped reads in fastq file

--- scripts/bowtie.sh
+++ scripts/bowtie.sh
@@ -5,8 +5,9 @@ set -e
 read1=$1
 read2=$2
 outputdir=$3
+numprocessors=$4
 
 module load bowtie2/2.2.7
 
 
-bowtie2 -p 4 --reorder -q -x /pylon5/mc5frap/kimkh415/713_team_4/data/bowtie_index/GRCh38_index -1 ${read1} -2 ${read2} -S ${outputdir}
+bowtie2 -p ${numprocessors} --reorder -q -x /pylon5/mc5frap/kimkh415/713_team_4/data/bowtie_index/GRCh38_index -1 ${read1} -2 ${read2} --un-conc ${outputdir}
