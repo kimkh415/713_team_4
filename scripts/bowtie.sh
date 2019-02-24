2d093c154d6f7b24b42792c9e9c45a940ea6a23c
Kircheis17 <yuhengz@andrew.cmu.edu>
2019-02-24 17:56:01 -0500

    update to accept index file

--- scripts/bowtie.sh
+++ scripts/bowtie.sh
@@ -4,10 +4,11 @@ set -e
 
 read1=$1
 read2=$2
-outputdir=$3
-numprocessors=$4
+index=$3
+outputdir=$4
+numprocessors=$5
 
 module load bowtie2/2.2.7
 
-
-bowtie2 -p ${numprocessors} -q -x /pylon5/mc5frap/kimkh415/713_team_4/data/bowtie_index/GRCh38_index -1 ${read1} -2 ${read2} --un-conc ${outputdir}/unmapped_%.fq --quiet > ${outputdir}/alignment.sam
+# /pylon5/mc5frap/kimkh415/713_team_4/data/bowtie_index/GRCh38_index
+bowtie2 -p ${numprocessors} -q -x ${index} -1 ${read1} -2 ${read2} --un-conc ${outputdir}/unmapped_%.fq --quiet > ${outputdir}/alignment.sam
