e3f1e972b8f4bf2f34bad569d26f2450fff84afd
Kwanho Kim <kimkh415@gmail.com>
2019-02-24 16:03:53 -0500

    modified salmon invoked in transrate

--- scripts/transrate.sh
+++ scripts/transrate.sh
@@ -5,5 +5,6 @@ left=$2
 right=$3
 output=$4
 threads=$5
-module load transrate
-transrate --assembly ${assembly} --left ${left} --right ${right} --output ${output} --threads ${threads} --loglevel error
+
+module load salmon/0.9.1
+/pylon5/mc5frap/kimkh415/713_team_4/bin/transrate/transrate --assembly ${assembly} --left ${left} --right ${right} --output ${output} --threads ${threads}
