8d57aa08412808f935677f64c38013cd561de5d4
Kwanho Kim <kimkh415@gmail.com>
2019-02-24 16:02:54 -0500

    now store output in SAM file

--- scripts/bowtie.sh
+++ scripts/bowtie.sh
@@ -10,4 +10,4 @@ numprocessors=$4
 module load bowtie2/2.2.7
 
 
-bowtie2 -p ${numprocessors} --reorder -q -x /pylon5/mc5frap/kimkh415/713_team_4/data/bowtie_index/GRCh38_index -1 ${read1} -2 ${read2} --un-conc ${outputdir}
+bowtie2 -p ${numprocessors} -q -x /pylon5/mc5frap/kimkh415/713_team_4/data/bowtie_index/GRCh38_index -1 ${read1} -2 ${read2} --un-conc ${outputdir}/unmapped_%.fq --quiet > ${outputdir}/alignment.sam
