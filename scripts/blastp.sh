66713bb2d9df2f3074bf68e41269ef129faef9ed
Kwanho Kim <kimkh415@gmail.com>
2019-02-24 17:17:44 -0500

    delete blastp

--- scripts/blastp.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-query=$1 # fasta file
-output=$2 # output filename
-entrez_query=$3 # Entrez query to restrict search. See: https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Web&PAGE_TYPE=BlastDocs&DOC_TYPE=BlastHelp#entrez_query
-
-module load blast/2.7.1
-
-blastp -db refseq_protein -query ${query} -entrez_query ${entrez_query} -remote -evalue 1e-04 -out ${output} # -outfmt 10
