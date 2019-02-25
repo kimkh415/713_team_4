55ea4ea6bf7ebf46ae2650c3c51751e0dd7f8181
Kwanho Kim <kimkh415@gmail.com>
2019-02-24 19:12:01 -0500

    modified directory of transcrate

--- scripts/transrate.sh
+++ scripts/transrate.sh
@@ -7,4 +7,4 @@ output=$4
 threads=$5
 
 module load salmon/0.9.1
-/pylon5/mc5frap/kimkh415/713_team_4/bin/transrate/transrate --assembly ${assembly} --left ${left} --right ${right} --output ${output} --threads ${threads}
+bin/transrate/transrate --assembly ${assembly} --left ${left} --right ${right} --output ${output} --threads ${threads}
